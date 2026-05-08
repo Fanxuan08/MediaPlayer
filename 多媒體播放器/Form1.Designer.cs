b8c00e67031958d613c261e9b7d5bf5fa1768146
Fanxuan08 <minna1008.su@gmail.com>
2026-05-08 13:31:16 +0800

    加入專案檔案。

---
+++ 多媒體播放器/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace 多媒體播放器
+{
+    partial class frmMediaPlayer
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMediaPlayer));
+            this.palButton = new System.Windows.Forms.Panel();
+            this.trackBarVolume = new System.Windows.Forms.TrackBar();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnBrowser = new System.Windows.Forms.Button();
+            this.wmpVideo = new AxWMPLib.AxWindowsMediaPlayer();
+            this.palButton.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // palButton
+            // 
+            this.palButton.BackColor = System.Drawing.Color.White;
+            this.palButton.Controls.Add(this.trackBarVolume);
+            this.palButton.Controls.Add(this.btnStop);
+            this.palButton.Controls.Add(this.btnPause);
+            this.palButton.Controls.Add(this.btnPlay);
+            this.palButton.Controls.Add(this.btnBrowser);
+            this.palButton.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.palButton.Location = new System.Drawing.Point(0, 376);
+            this.palButton.Name = "palButton";
+            this.palButton.Size = new System.Drawing.Size(792, 100);
+            this.palButton.TabIndex = 1;
+            // 
+            // trackBarVolume
+            // 
+            this.trackBarVolume.BackColor = System.Drawing.Color.Snow;
+            this.trackBarVolume.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBarVolume.Location = new System.Drawing.Point(540, 27);
+            this.trackBarVolume.Maximum = 100;
+            this.trackBarVolume.Name = "trackBarVolume";
+            this.trackBarVolume.Size = new System.Drawing.Size(200, 69);
+            this.trackBarVolume.TabIndex = 4;
+            this.trackBarVolume.TickFrequency = 10;
+            this.trackBarVolume.Value = 30;
+            this.trackBarVolume.Scroll += new System.EventHandler(this.trackBarVolume_Scroll);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnStop.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnStop.ForeColor = System.Drawing.Color.White;
+            this.btnStop.Location = new System.Drawing.Point(418, 27);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(101, 45);
+            this.btnStop.TabIndex = 3;
+            this.btnStop.Text = "停止";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPause.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPause.ForeColor = System.Drawing.Color.White;
+            this.btnPause.Location = new System.Drawing.Point(288, 27);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(101, 45);
+            this.btnPause.TabIndex = 2;
+            this.btnPause.Text = "暫停";
+            this.btnPause.UseVisualStyleBackColor = false;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnPlay.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnPlay.ForeColor = System.Drawing.Color.White;
+            this.btnPlay.Location = new System.Drawing.Point(158, 27);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(101, 45);
+            this.btnPlay.TabIndex = 1;
+            this.btnPlay.Text = "播放";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnBrowser
+            // 
+            this.btnBrowser.BackColor = System.Drawing.SystemColors.MenuHighlight;
+            this.btnBrowser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnBrowser.ForeColor = System.Drawing.Color.White;
+            this.btnBrowser.Location = new System.Drawing.Point(28, 27);
+            this.btnBrowser.Name = "btnBrowser";
+            this.btnBrowser.Size = new System.Drawing.Size(101, 45);
+            this.btnBrowser.TabIndex = 0;
+            this.btnBrowser.Text = "瀏覽";
+            this.btnBrowser.UseVisualStyleBackColor = false;
+            this.btnBrowser.Click += new System.EventHandler(this.btnBrowser_Click);
+            // 
+            // wmpVideo
+            // 
+            this.wmpVideo.Enabled = true;
+            this.wmpVideo.Location = new System.Drawing.Point(0, 0);
+            this.wmpVideo.Margin = new System.Windows.Forms.Padding(4);
+            this.wmpVideo.Name = "wmpVideo";
+            this.wmpVideo.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("wmpVideo.OcxState")));
+            this.wmpVideo.Size = new System.Drawing.Size(792, 379);
+            this.wmpVideo.TabIndex = 0;
+            // 
+            // frmMediaPlayer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(792, 476);
+            this.Controls.Add(this.wmpVideo);
+            this.Controls.Add(this.palButton);
+            this.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "frmMediaPlayer";
+            this.Text = "多媒體播放器";
+            this.Load += new System.EventHandler(this.frmMediaPlayer_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMediaPlayer_KeyDown);
+            this.palButton.ResumeLayout(false);
+            this.palButton.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBarVolume)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.wmpVideo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxWMPLib.AxWindowsMediaPlayer wmpVideo;
+        private System.Windows.Forms.Panel palButton;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnBrowser;
+        private System.Windows.Forms.TrackBar trackBarVolume;
+    }
+}
+
---
+++ 多媒體播放器/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 多媒體播放器
+{
+    public partial class frmMediaPlayer : Form
+    {
+        public frmMediaPlayer()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMediaPlayer_Load(object sender, EventArgs e)
+        {
+            wmpVideo.uiMode = "none";
+            // 設定播放器的初始音量與 TrackBar 的預設值同步
+            wmpVideo.settings.volume = trackBarVolume.Value;
+
+            // 確保表單可以接收按鍵事件
+            this.KeyPreview = true;
+            this.KeyDown += new KeyEventHandler(frmMediaPlayer_KeyDown);
+        }
+
+        private void btnBrowser_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "WMV files (*.wmv)|*.wmv|MP4 files (*.mp4)|*.mp4|AVI files (*.avi)|*.avi|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                wmpVideo.URL = ofd.FileName;
+                wmpVideo.Ctlcontrols.stop();
+            }
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.play();
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.pause();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            wmpVideo.Ctlcontrols.stop();
+        }
+
+        private void trackBarVolume_Scroll(object sender, EventArgs e)
+        {
+            // 將 Windows Media Player 的音量設定為 TrackBar 目前的值
+            wmpVideo.settings.volume = trackBarVolume.Value;
+        }
+
+        private void frmMediaPlayer_KeyDown(object sender, KeyEventArgs e)
+        {
+            double jumpSeconds = 5.0; // 設定每次快轉/倒退的秒數
+
+            if (e.KeyCode == Keys.Left)
+            {
+                // 倒退 5 秒 (保護機制：避免小於 0 秒導致錯誤)
+                if (wmpVideo.Ctlcontrols.currentPosition > jumpSeconds)
+                    wmpVideo.Ctlcontrols.currentPosition -= jumpSeconds;
+                else
+                    wmpVideo.Ctlcontrols.currentPosition = 0;
+
+                e.Handled = true;
+            }
+            else if (e.KeyCode == Keys.Right)
+            {
+                // 前進 5 秒
+                wmpVideo.Ctlcontrols.currentPosition += jumpSeconds;
+                e.Handled = true;
+            }
+        }
+    }
+}
